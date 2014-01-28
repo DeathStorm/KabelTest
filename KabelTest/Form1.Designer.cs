83b13b912ad95942821c5a0723f7e180898fcf3a
DeathStorm <ChristianT@OnlyEntertainment.de>
2014-01-28 15:46:59 +0100

    init

---
+++ KabelTest/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace KabelTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.field1_1 = new System.Windows.Forms.Button();
+            this.field1_2 = new System.Windows.Forms.Button();
+            this.field1_3 = new System.Windows.Forms.Button();
+            this.field1_4 = new System.Windows.Forms.Button();
+            this.field1_5 = new System.Windows.Forms.Button();
+            this.field1_6 = new System.Windows.Forms.Button();
+            this.field1_7 = new System.Windows.Forms.Button();
+            this.field1_8 = new System.Windows.Forms.Button();
+            this.field1_9 = new System.Windows.Forms.Button();
+            this.field1_0 = new System.Windows.Forms.Button();
+            this.field0_0 = new System.Windows.Forms.Button();
+            this.field0_9 = new System.Windows.Forms.Button();
+            this.field0_8 = new System.Windows.Forms.Button();
+            this.field0_7 = new System.Windows.Forms.Button();
+            this.field0_6 = new System.Windows.Forms.Button();
+            this.field0_5 = new System.Windows.Forms.Button();
+            this.field0_4 = new System.Windows.Forms.Button();
+            this.field0_3 = new System.Windows.Forms.Button();
+            this.field0_2 = new System.Windows.Forms.Button();
+            this.field0_1 = new System.Windows.Forms.Button();
+            this.SimulationsTimer = new System.Windows.Forms.Timer(this.components);
+            this.cbSimulationStatus = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(709, 230);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(709, 476);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // field1_1
+            // 
+            this.field1_1.Location = new System.Drawing.Point(68, 243);
+            this.field1_1.Name = "field1_1";
+            this.field1_1.Size = new System.Drawing.Size(50, 50);
+            this.field1_1.TabIndex = 2;
+            this.field1_1.Text = "button1";
+            this.field1_1.UseVisualStyleBackColor = true;
+            this.field1_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_2
+            // 
+            this.field1_2.Location = new System.Drawing.Point(124, 243);
+            this.field1_2.Name = "field1_2";
+            this.field1_2.Size = new System.Drawing.Size(50, 50);
+            this.field1_2.TabIndex = 3;
+            this.field1_2.Text = "button2";
+            this.field1_2.UseVisualStyleBackColor = true;
+            this.field1_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_3
+            // 
+            this.field1_3.Location = new System.Drawing.Point(180, 243);
+            this.field1_3.Name = "field1_3";
+            this.field1_3.Size = new System.Drawing.Size(50, 50);
+            this.field1_3.TabIndex = 4;
+            this.field1_3.Text = "button3";
+            this.field1_3.UseVisualStyleBackColor = true;
+            this.field1_3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_4
+            // 
+            this.field1_4.Location = new System.Drawing.Point(236, 243);
+            this.field1_4.Name = "field1_4";
+            this.field1_4.Size = new System.Drawing.Size(50, 50);
+            this.field1_4.TabIndex = 5;
+            this.field1_4.Text = "button4";
+            this.field1_4.UseVisualStyleBackColor = true;
+            this.field1_4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_5
+            // 
+            this.field1_5.Location = new System.Drawing.Point(292, 243);
+            this.field1_5.Name = "field1_5";
+            this.field1_5.Size = new System.Drawing.Size(50, 50);
+            this.field1_5.TabIndex = 6;
+            this.field1_5.Text = "button4";
+            this.field1_5.UseVisualStyleBackColor = true;
+            this.field1_5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_6
+            // 
+            this.field1_6.Location = new System.Drawing.Point(348, 243);
+            this.field1_6.Name = "field1_6";
+            this.field1_6.Size = new System.Drawing.Size(50, 50);
+            this.field1_6.TabIndex = 7;
+            this.field1_6.Text = "button4";
+            this.field1_6.UseVisualStyleBackColor = true;
+            this.field1_6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_7
+            // 
+            this.field1_7.Location = new System.Drawing.Point(404, 243);
+            this.field1_7.Name = "field1_7";
+            this.field1_7.Size = new System.Drawing.Size(50, 50);
+            this.field1_7.TabIndex = 8;
+            this.field1_7.Text = "button4";
+            this.field1_7.UseVisualStyleBackColor = true;
+            this.field1_7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_8
+            // 
+            this.field1_8.Location = new System.Drawing.Point(460, 243);
+            this.field1_8.Name = "field1_8";
+            this.field1_8.Size = new System.Drawing.Size(50, 50);
+            this.field1_8.TabIndex = 9;
+            this.field1_8.Text = "button4";
+            this.field1_8.UseVisualStyleBackColor = true;
+            this.field1_8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_9
+            // 
+            this.field1_9.Location = new System.Drawing.Point(516, 243);
+            this.field1_9.Name = "field1_9";
+            this.field1_9.Size = new System.Drawing.Size(50, 50);
+            this.field1_9.TabIndex = 10;
+            this.field1_9.Text = "button4";
+            this.field1_9.UseVisualStyleBackColor = true;
+            this.field1_9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field1_0
+            // 
+            this.field1_0.Location = new System.Drawing.Point(12, 243);
+            this.field1_0.Name = "field1_0";
+            this.field1_0.Size = new System.Drawing.Size(50, 50);
+            this.field1_0.TabIndex = 11;
+            this.field1_0.Text = "button4";
+            this.field1_0.UseVisualStyleBackColor = true;
+            this.field1_0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_0
+            // 
+            this.field0_0.Location = new System.Drawing.Point(12, 187);
+            this.field0_0.Name = "field0_0";
+            this.field0_0.Size = new System.Drawing.Size(50, 50);
+            this.field0_0.TabIndex = 21;
+            this.field0_0.Text = "button4";
+            this.field0_0.UseVisualStyleBackColor = true;
+            this.field0_0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_9
+            // 
+            this.field0_9.Location = new System.Drawing.Point(516, 187);
+            this.field0_9.Name = "field0_9";
+            this.field0_9.Size = new System.Drawing.Size(50, 50);
+            this.field0_9.TabIndex = 20;
+            this.field0_9.Text = "button4";
+            this.field0_9.UseVisualStyleBackColor = true;
+            this.field0_9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_8
+            // 
+            this.field0_8.Location = new System.Drawing.Point(460, 187);
+            this.field0_8.Name = "field0_8";
+            this.field0_8.Size = new System.Drawing.Size(50, 50);
+            this.field0_8.TabIndex = 19;
+            this.field0_8.Text = "button4";
+            this.field0_8.UseVisualStyleBackColor = true;
+            this.field0_8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_7
+            // 
+            this.field0_7.Location = new System.Drawing.Point(404, 187);
+            this.field0_7.Name = "field0_7";
+            this.field0_7.Size = new System.Drawing.Size(50, 50);
+            this.field0_7.TabIndex = 18;
+            this.field0_7.Text = "button4";
+            this.field0_7.UseVisualStyleBackColor = true;
+            this.field0_7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_6
+            // 
+            this.field0_6.Location = new System.Drawing.Point(348, 187);
+            this.field0_6.Name = "field0_6";
+            this.field0_6.Size = new System.Drawing.Size(50, 50);
+            this.field0_6.TabIndex = 17;
+            this.field0_6.Text = "button4";
+            this.field0_6.UseVisualStyleBackColor = true;
+            this.field0_6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_5
+            // 
+            this.field0_5.Location = new System.Drawing.Point(292, 187);
+            this.field0_5.Name = "field0_5";
+            this.field0_5.Size = new System.Drawing.Size(50, 50);
+            this.field0_5.TabIndex = 16;
+            this.field0_5.Text = "button4";
+            this.field0_5.UseVisualStyleBackColor = true;
+            this.field0_5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_4
+            // 
+            this.field0_4.Location = new System.Drawing.Point(236, 187);
+            this.field0_4.Name = "field0_4";
+            this.field0_4.Size = new System.Drawing.Size(50, 50);
+            this.field0_4.TabIndex = 15;
+            this.field0_4.Text = "button4";
+            this.field0_4.UseVisualStyleBackColor = true;
+            this.field0_4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_3
+            // 
+            this.field0_3.Location = new System.Drawing.Point(180, 187);
+            this.field0_3.Name = "field0_3";
+            this.field0_3.Size = new System.Drawing.Size(50, 50);
+            this.field0_3.TabIndex = 14;
+            this.field0_3.Text = "button3";
+            this.field0_3.UseVisualStyleBackColor = true;
+            this.field0_3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_2
+            // 
+            this.field0_2.Location = new System.Drawing.Point(124, 187);
+            this.field0_2.Name = "field0_2";
+            this.field0_2.Size = new System.Drawing.Size(50, 50);
+            this.field0_2.TabIndex = 13;
+            this.field0_2.Text = "button2";
+            this.field0_2.UseVisualStyleBackColor = true;
+            this.field0_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // field0_1
+            // 
+            this.field0_1.Location = new System.Drawing.Point(68, 187);
+            this.field0_1.Name = "field0_1";
+            this.field0_1.Size = new System.Drawing.Size(50, 50);
+            this.field0_1.TabIndex = 12;
+            this.field0_1.Text = "button1";
+            this.field0_1.UseVisualStyleBackColor = true;
+            this.field0_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Field_Click);
+            // 
+            // SimulationsTimer
+            // 
+            this.SimulationsTimer.Enabled = true;
+            this.SimulationsTimer.Interval = 1000;
+            this.SimulationsTimer.Tick += new System.EventHandler(this.SimulationsTimer_Tick);
+            // 
+            // cbSimulationStatus
+            // 
+            this.cbSimulationStatus.AutoSize = true;
+            this.cbSimulationStatus.Location = new System.Drawing.Point(709, 304);
+            this.cbSimulationStatus.Name = "cbSimulationStatus";
+            this.cbSimulationStatus.Size = new System.Drawing.Size(107, 17);
+            this.cbSimulationStatus.TabIndex = 22;
+            this.cbSimulationStatus.Text = "Simulation Aktiv?";
+            this.cbSimulationStatus.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 616);
+            this.Controls.Add(this.cbSimulationStatus);
+            this.Controls.Add(this.field0_0);
+            this.Controls.Add(this.field0_9);
+            this.Controls.Add(this.field0_8);
+            this.Controls.Add(this.field0_7);
+            this.Controls.Add(this.field0_6);
+            this.Controls.Add(this.field0_5);
+            this.Controls.Add(this.field0_4);
+            this.Controls.Add(this.field0_3);
+            this.Controls.Add(this.field0_2);
+            this.Controls.Add(this.field0_1);
+            this.Controls.Add(this.field1_0);
+            this.Controls.Add(this.field1_9);
+            this.Controls.Add(this.field1_8);
+            this.Controls.Add(this.field1_7);
+            this.Controls.Add(this.field1_6);
+            this.Controls.Add(this.field1_5);
+            this.Controls.Add(this.field1_4);
+            this.Controls.Add(this.field1_3);
+            this.Controls.Add(this.field1_2);
+            this.Controls.Add(this.field1_1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button field1_1;
+        private System.Windows.Forms.Button field1_2;
+        private System.Windows.Forms.Button field1_3;
+        private System.Windows.Forms.Button field1_4;
+        private System.Windows.Forms.Button field1_5;
+        private System.Windows.Forms.Button field1_6;
+        private System.Windows.Forms.Button field1_7;
+        private System.Windows.Forms.Button field1_8;
+        private System.Windows.Forms.Button field1_9;
+        private System.Windows.Forms.Button field1_0;
+        private System.Windows.Forms.Button field0_0;
+        private System.Windows.Forms.Button field0_9;
+        private System.Windows.Forms.Button field0_8;
+        private System.Windows.Forms.Button field0_7;
+        private System.Windows.Forms.Button field0_6;
+        private System.Windows.Forms.Button field0_5;
+        private System.Windows.Forms.Button field0_4;
+        private System.Windows.Forms.Button field0_3;
+        private System.Windows.Forms.Button field0_2;
+        private System.Windows.Forms.Button field0_1;
+        private System.Windows.Forms.Timer SimulationsTimer;
+        private System.Windows.Forms.CheckBox cbSimulationStatus;
+    }
+}
+
